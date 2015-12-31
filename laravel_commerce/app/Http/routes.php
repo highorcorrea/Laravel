c52f8b188d111d0174fcc8064e4f136d85c0495a
Highor Correa <highorcorrea@hotmail.com>
2015-12-31 09:24:45 -0200

    Alterado namespace para CodeCommerce

--- laravel_commerce/app/Http/routes.php
+++ laravel_commerce/app/Http/routes.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('exemplo',function(){
+    $nome = "Highor";
+    $sobrenome = "Correa";
+   return view('exemplo',compact('nome','sobrenome'));
+});
