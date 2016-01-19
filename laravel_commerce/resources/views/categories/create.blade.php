c8c69ae05b2d41af08bd7df688bd9617f20351a7
Highor Correa <highorcorrea@hotmail.com>
2016-01-19 18:27:48 -0200

    Testes e ajustes nas views - OK

--- laravel_commerce/resources/views/categories/create.blade.php
+++ laravel_commerce/resources/views/categories/create.blade.php
@@ -6,6 +6,7 @@
 
         @if ($errors->any())
                 <ul class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
                     @foreach($errors->all() as $error)
                         <li>{{ $error }}</li>
 
