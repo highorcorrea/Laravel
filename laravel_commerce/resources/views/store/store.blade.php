3f32b187194d2cc772e6d8317cdb70f2dfa78f84
Highor Correa <highorcorrea@hotmail.com>
2016-01-25 16:32:07 -0200

    Instalado elixir, assets fincionando OK

--- laravel_commerce/resources/views/store/store.blade.php
+++ laravel_commerce/resources/views/store/store.blade.php
@@ -8,12 +8,8 @@
     <title>Home | E-Shop</title>
 
 
-    <link href="{{ url('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{ url('css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{ url('css/prettyPhoto.css')}}" rel="stylesheet">
-    <link href="{{ url('css/animate.css')}}" rel="stylesheet">
-    <link href="{{ url('css/main.css')}}" rel="stylesheet">
-    <link href="{{ url('css/responsive.css')}}" rel="stylesheet">
+    <link href="{{ elixir('css/all.css')  }}" rel="stylesheet">
+
 
 
 </head><!--/head-->
@@ -146,11 +142,7 @@
 
 
 
-<script src="{{ url('js/jquery.js')}}"></script>
-<script src="{{ url('js/bootstrap.min.js')}}"></script>
-<script src="{{ url('js/jquery.scrollUp.min.js')}}"></script>
-<script src="{{ url('js/jquery.prettyPhoto.js')}}"></script>
-<script src="{{ url('js/main.js')}}"></script>
+<script src="{{ elixir('js/all.js')}}"></script>
 
 </body>
 </html>
